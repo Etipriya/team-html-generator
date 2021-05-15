ba897066deec6215041c84866a49fdf7e0be22a1
Eti Priya <etipriya@gmail.com>
2021-05-15 22:49:02 +0100

    styling changed and async await used

--- src/index.js
+++ src/index.js
@@ -13,69 +13,65 @@ function initApp() {
 }
 
 //Declare a function to add the member using inquirer and prompt
-function addMember() {
-  inquirer
-    .prompt([
-      {
-        message: "Enter team member's name",
-        name: "name",
-      },
-      {
-        type: "list",
-        message: "Select team member's role",
-        choices: ["Engineer", "Intern", "Manager"],
-        name: "role",
-      },
-      {
-        message: "Enter team member's id",
-        name: "id",
-      },
-      {
-        message: "Enter team member's email address",
-        name: "email",
-      },
-    ])
-    .then(function ({ name, role, id, email }) {
-      let roleInfo = "";
-      if (role === "Engineer") {
-        roleInfo = "GitHub username";
-      } else if (role === "Intern") {
-        roleInfo = "school name";
-      } else {
-        roleInfo = "office phone number";
-      }
-      inquirer
-        .prompt([
-          {
-            message: `Enter team member's ${roleInfo}`,
-            name: "roleInfo",
-          },
-          {
-            type: "list",
-            message: "Would you like to add more team members?",
-            choices: ["yes", "no"],
-            name: "moreMembers",
-          },
-        ])
-        .then(function ({ roleInfo, moreMembers }) {
-          let newMember;
-          if (role === "Engineer") {
-            newMember = new Engineer(name, id, email, roleInfo);
-          } else if (role === "Intern") {
-            newMember = new Intern(name, id, email, roleInfo);
-          } else {
-            newMember = new Manager(name, id, email, roleInfo);
-          }
-          employees.push(newMember);
-          addHtml(newMember).then(function () {
-            if (moreMembers === "yes") {
-              addMember();
-            } else {
-              finishHtml();
-            }
-          });
-        });
-    });
+async function addMember() {
+  const answers = await inquirer.prompt([
+    {
+      message: "Enter team member's name",
+      name: "name",
+    },
+    {
+      type: "list",
+      message: "Select team member's role",
+      choices: ["Engineer", "Intern", "Manager"],
+      name: "role",
+    },
+    {
+      message: "Enter team member's id",
+      name: "id",
+    },
+    {
+      message: "Enter team member's email address",
+      name: "email",
+    },
+  ]);
+  const { name, role, id, email } = answers;
+  let roleInfo = "";
+  if (role === "Engineer") {
+    roleInfo = "GitHub username";
+  } else if (role === "Intern") {
+    roleInfo = "school name";
+  } else {
+    roleInfo = "office phone number";
+  }
+  const response = await inquirer.prompt([
+    {
+      message: `Enter team member's ${roleInfo}`,
+      name: "roleInfo",
+    },
+    {
+      type: "list",
+      message: "Would you like to add more team members?",
+      choices: ["yes", "no"],
+      name: "moreMembers",
+    },
+  ]);
+  //const { roleInfo, moreMembers } = response;
+  let newMember;
+  if (role === "Engineer") {
+    newMember = new Engineer(name, id, email, response.roleInfo);
+  } else if (role === "Intern") {
+    newMember = new Intern(name, id, email, response.roleInfo);
+  } else {
+    newMember = new Manager(name, id, email, response.roleInfo);
+  }
+  employees.push(newMember);
+  addHtml(newMember).then(function () {
+    if (response.moreMembers === "yes") {
+      addMember();
+    } else {
+      finishHtml();
+    }
+  });
 }
 
 // function renderHtml(memberArray) {
@@ -110,7 +106,7 @@ function startHtml() {
       console.log(err);
     }
   });
-  console.log("start");
+  console.log("file created");
 }
 
 // Declare a function which contains the HTMl for adding engineer, intern and manager
@@ -125,11 +121,11 @@ function addHtml(member) {
       const gitHub = member.getGithub();
       data = `<div class="col-6">
             <div class="card mx-auto mb-3" style="width: 18rem">
-            <h5 class="card-header">${name}<br /><br />Engineer</h5>
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item">ID: ${id}</li>
-                <li class="list-group-item">Email Address: ${email}</li>
-                <li class="list-group-item">GitHub: ${gitHub}</li>
+            <div class="card-header bg-success">${name}<br /><br />Engineer</div>
+            <ul class="list-group list-group-flush ">
+                <li class="list-group-item bg-secondary text-light">ID: ${id}</li>
+                <li class="list-group-item bg-secondary text-light">Email Address: ${email}</li>
+                <li class="list-group-item bg-secondary text-light">GitHub: ${gitHub}</li>
             </ul>
             </div>
         </div>`;
@@ -137,11 +133,11 @@ function addHtml(member) {
       const school = member.getSchool();
       data = `<div class="col-6">
             <div class="card mx-auto mb-3" style="width: 18rem">
-            <h5 class="card-header">${name}<br /><br />Intern</h5>
+            <div class="card-header bg-success">${name}<br /><br />Intern</div>
             <ul class="list-group list-group-flush">
-                <li class="list-group-item">ID: ${id}</li>
-                <li class="list-group-item">Email Address: ${email}</li>
-                <li class="list-group-item">School: ${school}</li>
+                <li class="list-group-item bg-secondary text-light">ID: ${id}</li>
+                <li class="list-group-item bg-secondary text-light">Email Address: ${email}</li>
+                <li class="list-group-item bg-secondary text-light">School: ${school}</li>
             </ul>
             </div>
         </div>`;
@@ -149,11 +145,11 @@ function addHtml(member) {
       const officePhone = member.getOfficeNumber();
       data = `<div class="col-6">
             <div class="card mx-auto mb-3" style="width: 18rem">
-            <h5 class="card-header">${name}<br /><br />Manager</h5>
+            <div class="card-header bg-success">${name}<br /><br />Manager</div>
             <ul class="list-group list-group-flush">
-                <li class="list-group-item">ID: ${id}</li>
-                <li class="list-group-item">Email Address: ${email}</li>
-                <li class="list-group-item">Office Phone: ${officePhone}</li>
+                <li class="list-group-item bg-secondary text-light">ID: ${id}</li>
+                <li class="list-group-item bg-secondary text-light">Email Address: ${email}</li>
+                <li class="list-group-item bg-secondary text-light">Office Phone: ${officePhone}</li>
             </ul>
             </div>
         </div>`;
